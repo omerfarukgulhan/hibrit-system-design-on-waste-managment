9f9c1f8f4e932aa965a5b831342113b43aed003f
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-06-03 21:13:25 +0300

    initial data file updated due to wrong coordinates

---
+++ server_node/util/asd.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const Bin = require("../models/binModel");
+
+const token =
+  "pk.eyJ1IjoibGVyaXRoIiwiYSI6ImNsdnIyZmh6cDBnZXYya21oZGFxendvcWsifQ.Qhm_zr1bKU_Jkuk8HSr80w";
+
+async function calculateGraph(dataList) {
+  const directionsList = [];
+
+  for (let i = 0; i < dataList.length; i++) {
+    for (let j = 0; j < dataList.length; j++) {
+      try {
+        const response = await axios.get(
+          `https://api.mapbox.com/directions/v5/mapbox/driving/${dataList[i].bin_lat},${dataList[i].bin_lng};${dataList[j].bin_lat},${dataList[j].bin_lng}?steps=true&geometries=geojson&access_token=${token}`
+        );
+        if (response.status === 200) {
+          directionsList.push(response.data);
+        } else {
+          console.error(`Error: ${response.status}, ${response.data}`);
+          return;
+        }
+      } catch (error) {
+        console.error(
+          `Error: ${
+            error.response ? error.response.status : "Network error"
+          }, ${error.message}`
+        );
+        return;
+      }
+    }
+  }
+
+  const matrix = Array(dataList.length)
+    .fill(null)
+    .map(() => Array(dataList.length).fill(0));
+  let index = 0;
+
+  for (let i = 0; i < dataList.length; i++) {
+    for (let j = 0; j < dataList.length; j++) {
+      matrix[i][j] = directionsList[index].routes[0].duration;
+      index += 1;
+    }
+  }
+
+  return matrix;
+}
+
+module.exports = calculateGraph;
